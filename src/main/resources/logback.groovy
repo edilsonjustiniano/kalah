c3f104038af40de8337fdb575a7da5003a0b4ff7
Edilson Justiniano <edilsonjustiniano@gmail.com>
2019-10-14 09:47:51 -0300

    Improving the loogback setup

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,10 +1,12 @@
-def appName = "kalah-game"
+import static ch.qos.logback.classic.Level.INFO
+import static ch.qos.logback.classic.Level.DEBUG
 
-appender("dev_stdout", ConsoleAppender) {
-    target = "System.out"
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%d{yyyy-MM-dd HH:mm:ss} [%thread] %-5level %logger{50} - %msg%n"
     }
 }
-
-root(INFO, ["dev_stdout"])
+root(INFO, ["CONSOLE"])
